1f7432673b4a0c5ded731b8f82c3bfcc35ebd439
LeonidSennik <barodfsh@gmail.com>
2025-09-24 15:43:12 +0300

    df

--- app/(private routes)/notes/filter/page.tsx
+++ app/(private routes)/notes/filter/page.tsx
@@ -1,5 +1,5 @@
 import { HydrationBoundary, dehydrate, QueryClient } from '@tanstack/react-query';
-import { fetchNotes } from '../../../../lib/api/api';
+import { fetchNotes } from '../../../../lib/api/serverApi';
 import NotesClient from './Notes.client';
 
 export default async function AllNotesPage() {
